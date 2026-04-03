dad58fae7190c4278c8c6663bd6910073b67d9ba
veerkunwar <veerkunwarsingh19296@gmail.com>
2026-04-04 00:04:27 +0530

    clean project without secrets

---
+++ frontend/src/pages/Doctor.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DoctorPage from '../components/DoctorPage'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Doctor = () => {
+  return (
+    <div>
+        <Navbar />
+      <DoctorPage />
+      <Footer/>
+    </div>
+  )
+}
+
+export default Doctor
