9737f125e73baefca36d6a17ad3fa452079328a6
veer kunwar singh <149755833+Veerkunwar@users.noreply.github.com>
2026-04-04 00:56:17 +0530

    Update API_BASE URL to production endpoint

--- admin/src/components/AddService.jsx
+++ admin/src/components/AddService.jsx
@@ -13,7 +13,7 @@ import {
 } from "lucide-react";
 
 const AddService = ({ serviceId }) => {
-  const API_BASE = "http://localhost:4000";
+  const API_BASE = https://health-lrjl.onrender.com";
 
   const fileRef = useRef(null);
   const [imagePreview, setImagePreview] = useState(null);
